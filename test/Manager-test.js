9d4d5f7dfd131b240a2ee8793637527e46919b79
Virginia Ladd <virginialadd2@gmail.com>
2020-01-08 16:00:11 -0700

    Write out testing skeleton for Manager

---
+++ test/Manager-test.js
@@ -0,0 +1,51 @@
+import chai from 'chai';
+const expect = chai.expect;
+
+// import Manager from '../src/classes/Manager'
+
+describe('Manager', function() {
+  let manager;
+
+  beforeEach(() => {
+    // manager = new Manager('manager', 'overlook2019');
+  });
+
+  // it('should have access to the Manager class', function() {
+  //   expect(manager).to.be.an.instanceOf(Manager);
+  // });
+
+  // it('should have a Booking ID', function() {
+  //   expect(manager.name).to.equal('manager');
+  // });
+
+  // it('should have a User ID', function() {
+  //   expect(manager.password).to.equal('overlook2019');
+  // });
+
+  it('should Be able to SearchForUser', function() {
+  });
+
+  it('should be able to addRoomForUser', function() {
+  });
+
+  it('should be able to deleteUsersReservation', function() {
+  });
+
+  it('should checkOccupiedRooms', function() {
+  });
+
+  it('should be able to totalDaysRevenue', function() {
+  });
+
+  it('should be able to checkRoomAvailability', function() {
+  });
+
+  it('should be able to displayApology', function() {
+  });
+
+  describe('', function() {
+
+  });
+
+
+});
