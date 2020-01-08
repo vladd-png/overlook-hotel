dce4139cc0354f0d0fd0813338a09a05e21cc829
Virginia Ladd <virginialadd2@gmail.com>
2020-01-08 15:10:44 -0700

    Add classes for Rooms, User, and test-files for classes

---
+++ src/classes/Rooms.js
@@ -0,0 +1,12 @@
+class Room {
+  constructor(type,) {
+    this.number = number;
+    this.roomType = roomType;
+    this.bidet = bidet;
+    this.bedSize = bedSize;
+    this.numBeds = numBeds;
+    this.costPerNight = costPerNight;
+  }
+}
+
+module.exports = Room;
---
+++ src/classes/User.js
@@ -0,0 +1,21 @@
+class User {
+  constructor(name, id) {
+    this.name = name;
+    this.id = id;
+    this.pastBookings = [];
+  }
+  accessLogin() {
+
+  }
+  bookRoom() {
+
+  }
+  totalSpentMoney() {
+
+  }
+  checkPastBookings() {
+    
+  }
+}
+
+module.exports = User;
