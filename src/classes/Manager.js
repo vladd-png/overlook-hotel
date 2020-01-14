50f8cae6a591099611844d5b9ae6c593c59e9501
Virginia Ladd <virginialadd2@gmail.com>
2020-01-13 19:25:58 -0700

    update frontdesk tests

--- src/classes/Manager.js
+++ src/classes/Manager.js
@@ -1,20 +1,20 @@
-import { } from '../index.js'
-import User from './User.js'
+// import { } from '../index.js'
+// import User from './User.js'
 
-class Manager extends User {
+class Manager {
   constructor(data, name) {
-    super(data);
+    // super(data);
     this.name = name;
     this.allUsers = [];
   }
-  // createUserData(user) {
-  //   this.allUsers.push(user)
-  // }
-  // searchForUser(userNum) {
-  //   return this.allUsers.filter(user => {
-  //     return user.id === userNum;
-  //   })
-  // }
+  createUserData(user) {
+    this.allUsers.push(user)
+  }
+  searchForUser(userNum) {
+    return this.allUsers.filter(user => {
+      return user.id === userNum;
+    })
+  }
   addRoomForUser() {
   }
   deleteUsersReservation() {
