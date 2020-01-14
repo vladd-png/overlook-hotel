de204c936a4a36eb0068314b95a0e48441df04c3
Virginia Ladd <virginialadd2@gmail.com>
2020-01-14 11:28:09 -0700

    add spies for manager testing

--- test/Manager-test.js
+++ test/Manager-test.js
@@ -1,18 +1,18 @@
 import chai from 'chai';
 const expect = chai.expect;
-// const spies = require('chai-spies');
-// chai.use(spies);
-
-// chai.spy.on(frontdesk, [totalDaysRevenue, findFullRooms], () => {});
-
+const spies = require('chai-spies');
+chai.use(spies);
+import domUpdates from '../src/DOMupdates.js';
 import Manager from '../src/classes/Manager'
-// import User from '../src/classes/User';
+import User from '../src/classes/User';
+
 
 
 describe('Manager', function() {
   let manager, user, allBookings, room1, room2, room3, room4, allRooms;
 
   beforeEach(() => {
+    user = new User('manager', '51')
     manager = new Manager('manager', 'overlook2019'),
     room1 = { number: 15, roomType: "residential suite", bidet: false, bedSize: "full", numBeds: 1, costPerNight: 294.56},
     room2 = { number: 24, roomType: "suite", bidet: false, bedSize: "queen", numBeds: 1, costPerNight: 327.24},
@@ -47,18 +47,18 @@ describe('Manager', function() {
     }
   })
 
+  it('should show date on the page', function() {
+    chai.spy.on(domUpdates, ['showDateForManager'], () => {});
+    manager.showDate('2020/01/10');
+    expect(domUpdates.showDateForManager).to.have.been.called(1);
+  });
+
   it('should Be able to SearchForUser', function() {
     manager.createUserData(user);
     expect(manager.searchForUser(user.id)).to.deep.equal([user])
   });
 
-  it('should be able to addRoomForUser', function() {
-  });
-
-  it('should be able to deleteUsersReservation', function() {
-  });
-
-  it('should be able to displayApology', function() {
+  it.skip('should be able to displayApology', function() {
   });
 
 });
